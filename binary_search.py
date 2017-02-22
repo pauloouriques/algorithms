dc03fa78830e305b08566ec2c0e897a72e870259
Paulo Ouriques <ourixilva@gmail.com>
2017-02-22 18:34:56 -0300

    Some changes to the files names.

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,37 @@
 
+def solution(A, B, C):
+    mapping = {}
+    aux = {}
+
+    for i in xrange(len(A)):
+        aux[i] = i
+        for j in xrange(A[i], B[i] + 1):
+            if j in mapping:
+                mapping[j].append(i)
+            else:
+                mapping[j] = []
+                mapping[j].append(i)
+
+    count = 0
+
+    for i in xrange(len(C)):
+        if C[i] in mapping:
+            for e in mapping[C[i]]:
+                if e in aux:
+                    aux.pop(e)
+        count += 1
+        if len(aux) == 0:
+            return count
+
+    return -1
+
+print solution([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2])
+
+
+
+
+
+
 def binary_search(A, x):
     n = len(A)
     beg = 0
--- files.py
+++ files.py
@@ -1,24 +1,41 @@
-def solution(S):
-    stack = []
-    map = {'{':'}', '[':']', '(':')'}
-    for s in S:
-        if len(stack) == 0:
-            if s in map.values():
-                return 0
-            stack.append(s)
-        else:
-            last = stack.pop()
-            if s == map[last]:
-                pass
-            else:
-                if s in map.values():
-                    return 0
-                else:
-                    stack.append(last)
-                    stack.append(s)
+def read_file():
+    file = open("file", "r")
+    for line in file:
+        print line.replace("\n", "")
 
-    if len(stack) == 0:
-        return 1
-    return 0
+read_file()
 
-print solution('()()')
+
+
+
+
+
+
+
+
+
+
+# def solution(S):
+#     stack = []
+#     map = {'{':'}', '[':']', '(':')'}
+#     for s in S:
+#         if len(stack) == 0:
+#             if s in map.values():
+#                 return 0
+#             stack.append(s)
+#         else:
+#             last = stack.pop()
+#             if s == map[last]:
+#                 pass
+#             else:
+#                 if s in map.values():
+#                     return 0
+#                 else:
+#                     stack.append(last)
+#                     stack.append(s)
+#
+#     if len(stack) == 0:
+#         return 1
+#     return 0
+#
+# print solution('()()')
